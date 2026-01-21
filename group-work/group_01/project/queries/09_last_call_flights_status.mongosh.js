d34809f04d11a88c46b17422de4e1bd3bb2440e8
Petúnia Dias <psofia20056@gmail.com>
2026-01-21 14:04:51 +0000

    feat: implement getLastCallFlightsStatus function to retrieve last call flights

--- group-work/group_01/project/queries/09_last_call_flights_status.mongosh.js
+++ group-work/group_01/project/queries/09_last_call_flights_status.mongosh.js
@@ -1,1 +1,21 @@
-db = db.getSiblingDB("group_01_flight_management_system_final");
+/**
+ * Get last call flights status
+ * @param {Object} db - MongoDB database instance 
+ */
+async function getLastCallFlightsStatus(
+    db,
+) {
+    try {
+        const result = await db.collection("flights")
+            .find({ status: 3 })
+            .sort({ departureTime: 1})
+            .toArray();
+        console.log("Last call flights status:", result);
+        return result;
+    } catch (error) {
+        console.error("Error finding last call flights status:", error);
+        throw error;
+    }
+}
+
+module.exports = { getLastCallFlightsStatus };
