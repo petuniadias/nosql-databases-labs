f4d4f5bd4025333d952694ac6b1c38436cef83e0
Petúnia Dias <psofia20056@gmail.com>
2026-01-21 13:15:41 +0000

    feat: implement list airports in city query

--- group-work/group_01/project/queries/07_list_airports_in_city.mongosh.js
+++ group-work/group_01/project/queries/07_list_airports_in_city.mongosh.js
@@ -1,1 +1,22 @@
-db = db.getSiblingDB("group_01_flight_management_system_final");
+async function listAirportsInCity(
+    db,
+    municipality = "Porto"
+) {
+    try {
+        const result = await db.collection("airports").aggregate([
+            {
+                $match: { municipality }
+            },
+            {
+                $project: { _id: 0, ident: 1, name: 1, country: 1 }
+            }
+        ]).toArray();
+
+        console.log(`List airports in ${municipality}:`, result);
+    } catch (error) {
+        console.error("Error listing airports in city", error);
+        throw error;
+    }
+}
+
+module.exports = { listAirportsInCity };
